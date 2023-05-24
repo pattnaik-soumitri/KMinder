7ba52f2d4d0176ae802d8a4a6dffd7c0c019c47f
pattnaik_soumitri <soumitri.2.pattnaik@shoppertrak.com>
2023-05-25 02:30:30 +0530

    - Added primevue component library.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    css: [
+        'primevue/resources/themes/saga-blue/theme.css',
+        'primevue/resources/primevue.css',
+        'primeicons/primeicons.css',
+        'primeflex/primeflex.css'
+      ],
+      build: {
+        transpile: ['primevue']
+      },
 })
