991df04b99bee092e1aae0a39307748e69bf8271
pattnaik-soumitri <soumitri.pattnaik@gmail.com>
2023-07-02 18:53:16 +0530

    - Added app layout sceleton

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <template>
-  <header>
-  </header>
 
-  <main>
-    <RouterView />
-  </main>
+  <div class="container">
+    <header>
+        <nav>
+          <!-- BRAND -->
+          <ul>
+            <li><router-link to="/"><strong>KMinder</strong></router-link></li>
+          </ul>
 
-  <foorter></foorter>
+          <!-- NAVS -->
+          <ul>
+            <li><router-link to="about">About</router-link></li>
+          </ul>
+        </nav>
+    </header>
+    
+    <!-- CONTENT -->
+    <main>
+      <RouterView />
+    </main>
+    
+    <!-- FOOTER -->
+    <footer></footer>
+  </div>
+
 </template>
+
+<style scoped>
+header {
+  border-bottom: solid grey 1px;
+}
+</style>
