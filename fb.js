093f64ae60a0f132a31491e6c283e667832fee0e
pattnaik-soumitri <soumitri.pattnaik@gmail.com>
2023-07-02 19:21:52 +0530

    - Added firebase auth

--- fb.js
+++ fb.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBXG_bfJZWFKdlwPahqTL2eUptQgBsHirk",
@@ -17,3 +18,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
